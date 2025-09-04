d2dd68a708197a8ea54ec291919e329d23d8c462
Juan Quijano <listas@juanquijano.es>
2025-09-04 10:52:40 +0000

    Desafio: formato y visualización de las instrucciones

--- Program.cs
+++ Program.cs
@@ -2,17 +2,11 @@
 
 Console.Clear();
 
-var nombre = "Bob";
-var saludo = "Hola";
-var mensaje = $"{saludo} {nombre}! Me alegro volver a verte {nombre}.";
-Console.WriteLine(mensaje);
+string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 
-Console.WriteLine();
-var version = 11;
-var textoActualizacion = "Actualiza a Windows";
-mensaje = $"{textoActualizacion} {version}!";
-Console.WriteLine(mensaje);
-
-Console.WriteLine();
-var nombreProyecto = "Primer-Projecto";
-Console.WriteLine($@"C:\Output\{nombreProyecto}\Data");
+Console.WriteLine($@"View English output:
+  c:\Exercise\{projectName}\data.txt
+  ");
+Console.WriteLine($@"{russianMessage}:
+  c:\Exercise\{projectName}\ru-RU\data.txt");
