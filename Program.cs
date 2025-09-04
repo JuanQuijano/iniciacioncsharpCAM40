9ef5f5387abba577ee2218aa5e254832ffb1860c
Juan Quijano <listas@juanquijano.es>
2025-09-04 10:47:24 +0000

    Interpolacion de cadenas

--- Program.cs
+++ Program.cs
@@ -4,5 +4,15 @@ Console.Clear();
 
 var nombre = "Bob";
 var saludo = "Hola";
-var mensaje = saludo + " " + nombre + "!";
+var mensaje = $"{saludo} {nombre}! Me alegro volver a verte {nombre}.";
 Console.WriteLine(mensaje);
+
+Console.WriteLine();
+var version = 11;
+var textoActualizacion = "Actualiza a Windows";
+mensaje = $"{textoActualizacion} {version}!";
+Console.WriteLine(mensaje);
+
+Console.WriteLine();
+var nombreProyecto = "Primer-Projecto";
+Console.WriteLine($@"C:\Output\{nombreProyecto}\Data");
