5c04d6673afa9f0f864d46683b52fb5167afc8cb
Juan Quijano <listas@juanquijano.es>
2025-09-04 10:58:11 +0000

    sumas con conversión implícita de datos

--- Program.cs
+++ Program.cs
@@ -2,11 +2,9 @@
 
 Console.Clear();
 
-string projectName = "ACME";
-string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+var nombre = "Pepe";
+var primerNumero = 12;
+var segundoNumero = 7;
 
-Console.WriteLine($@"View English output:
-  c:\Exercise\{projectName}\data.txt
-  ");
-Console.WriteLine($@"{russianMessage}:
-  c:\Exercise\{projectName}\ru-RU\data.txt");
+Console.WriteLine(nombre + " compró " + (primerNumero + 10) + " manzanas.");
+
