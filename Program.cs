cf228cf12e62c1d0a968ff3e26ddd30986546a2b
Juan Quijano <listas@juanquijano.es>
2025-09-04 09:48:01 +0000

    Declaración de variables

--- Program.cs
+++ Program.cs
@@ -1,20 +1,13 @@
 ﻿Console.Clear();
 
-Console.WriteLine('H');
-Console.WriteLine("Hola Mundo");
-Console.WriteLine("1234");
-Console.WriteLine(1234);
-Console.WriteLine(0.25F);
-Console.WriteLine(2.625);
-Console.WriteLine(12.39816m);
-Console.WriteLine(true);
-Console.WriteLine(false);
-Console.WriteLine("False");
+var firstName = "Juan";
+Console.WriteLine("Hello, " + firstName + "!");
 
-const int z = 1;
-var precioNeto = 53;
-int iva = 21 * precioNeto / 100;
+firstName = 1.ToString();
+Console.WriteLine("Hello, " + firstName + "!");
 
-var precioFinal = precioNeto + iva;
+firstName = "Bob";
+Console.WriteLine("Hello, " + firstName + "!");
 
-Console.WriteLine(precioFinal);
+firstName = "Pepe";
+Console.WriteLine("Hello, " + firstName + "!");
