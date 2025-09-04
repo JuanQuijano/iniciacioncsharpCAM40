2e661a2d92cc36decb06c5571b99f96a20ef8d7a
Juan Quijano <listas@juanquijano.es>
2025-09-04 11:11:02 +0000

    Realizacio de operaciones matemáticas

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿using System.Runtime.ExceptionServices;
+﻿Console.Clear();
 
-Console.Clear();
+var suma = 7 + 5;
+var resta = 7 - 5;
+var multiplicacion = 7 * 5;
+var division = 7 / 5;
 
-var nombre = "Pepe";
-var primerNumero = 12;
-var segundoNumero = 7;
+Console.WriteLine("Suma: " + suma);
+Console.WriteLine("Resta: " + resta);
+Console.WriteLine("Multiplicación: " + multiplicacion);
+Console.WriteLine("División: " + division);
 
-Console.WriteLine(nombre + " compró " + (primerNumero + 10) + " manzanas.");
 
+var divivendo = 233;
+var divisor = 5;
+var divisionDecimal = (decimal)divivendo / divisor;
+Console.WriteLine("División decimal: " + divisionDecimal);
+Console.WriteLine("Resto: " + (decimal)divivendo % divisor);  
+
+/*PEMDAS
+Parentesis
+Exponentes
+Multiplicación
+División
+Suma y resta
+
+de izquierda a derecha
+*/
+
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
