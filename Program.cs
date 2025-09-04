42344e895b588d05b7e3c0d94cce0cd0906a8179
Juan Quijano <listas@juanquijano.es>
2025-09-04 09:53:10 +0000

    Desafio: mostrar valores literales y variables

--- Program.cs
+++ Program.cs
@@ -1,13 +1,7 @@
 ﻿Console.Clear();
 
-var firstName = "Juan";
-Console.WriteLine("Hello, " + firstName + "!");
-
-firstName = 1.ToString();
-Console.WriteLine("Hello, " + firstName + "!");
-
-firstName = "Bob";
-Console.WriteLine("Hello, " + firstName + "!");
-
-firstName = "Pepe";
-Console.WriteLine("Hello, " + firstName + "!");
+var nombre = "Bob";
+var numEmails = 3;
+var temperatura = 34.4;
+var mensaje = $"Hola {nombre}, tienes {numEmails} emails en tu buzón de correo, y la temperatura es {temperatura}C";
+Console.WriteLine(mensaje);
