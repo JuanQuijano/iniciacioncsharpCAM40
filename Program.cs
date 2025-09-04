fbfb4d8a141ec2134bc797136e726f0de46a510c
Juan Quijano <listas@juanquijano.es>
2025-09-04 09:33:28 +0000

    Literales y declarar variables

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
 ﻿Console.Clear();
 
-Console.WriteLine("Esta es la primera linea");
-Console.WriteLine("Esta es la segunda linea");
+Console.WriteLine('H');
+Console.WriteLine("Hola Mundo");
+Console.WriteLine("1234");
+Console.WriteLine(1234);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine("False");
+
+const int z = 1;
+var precioNeto = 53;
+int iva = 21 * precioNeto / 100;
+
+var precioFinal = precioNeto + iva;
+
+Console.WriteLine(precioFinal);
