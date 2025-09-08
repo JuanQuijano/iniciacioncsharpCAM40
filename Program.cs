73e4057e7a6f77b3d01300201d238dad55535855
Juan Quijano <listas@juanquijano.es>
2025-09-08 11:21:21 +0000

    Ejercicio: Implementación del operador condicional

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
 ﻿Console.Clear();
 
-var a = 7;
-var b = 6;
-Console.WriteLine(a != b); // output: True
+var saleAmount = 999;
+var discount = (saleAmount > 1000) ? 100 : 50;
 
-var s1 = "Hello";
-var s2 = "Hello";
-Console.WriteLine(s1 != s2); // output: False
-Console.WriteLine(!(false)); // output: True
+Console.WriteLine($"Discount: {discount}");
