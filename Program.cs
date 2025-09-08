5c0054e592b9dc6e08e90ac0f762d540e99576ff
Juan Quijano <listas@juanquijano.es>
2025-09-08 09:34:24 +0000

    Desafío de código: aplicación de directrices de estilo para mejorar la legibilidad

--- Program.cs
+++ Program.cs
@@ -1,24 +1,18 @@
-﻿Console.Clear();
+﻿using System.Linq;
 
-Random dice = new Random();
+Console.Clear();
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-int total = roll1 + roll2 + roll3;
+/* Muestro el número de veces que aparece una letra en una frase que muestro al reves*/
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+var frase = "The quick brown fox jumps over the lazy dog.";
+var fraseAlReves = frase.Reverse();
+int numeroDeVecesAparece = 0;
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+foreach (var letra in fraseAlReves)
 {
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
+    if (letra == 'o')
+    { numeroDeVecesAparece++; }
 }
+
+Console.WriteLine(string.Concat(fraseAlReves));
+Console.WriteLine($"'o' appears {numeroDeVecesAparece} times.");
