aab2a51827a52326d308bf46d3c2d4712dedea97
Juan Quijano <listas@juanquijano.es>
2025-09-09 09:03:37 +0000

    Ejercicio: Creación y configuración para los bucles de iteración

--- Program.cs
+++ Program.cs
@@ -1,57 +1,44 @@
 ﻿Console.Clear();
- 
-// SKU = Stock Keeping Unit. 
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
-string[] product = sku.Split('-');
-
-string type = product[0];
-string color = product[1];
-string size = product[2];
-
-switch (type)
+/*
+for (var indice = 0; indice < 10; indice++)
 {
-    case "01":
-        type = "Sweat shirt";
-        break;
-    case "02":
-        type = "T-Shirt";
-        break;
-    case "03":
-        type = "Sweat pants";
-        break;
-    default:
-        type = "Other";
-        break;
+    Console.WriteLine(indice);
 }
 
-switch (color)
+for (int i = 10; i >= 0; i--)
 {
-    case "BL":
-        color = "Black";
-        break;
-    case "MN":
-        color = "Maroon";
-        break;
-    default:
-        color = "White";
-        break;
+    Console.WriteLine(i);
 }
 
-switch (size)
+for (int i = 0; i < 10; i += 3)
 {
-    case "S":
-        size = "Small";
-        break;
-    case "M":
-        size = "Medium";
-        break;
-    case "L":
-        size = "Large";
-        break;
-    default:
-        size = "One Size Fits All";
-        break;
+    Console.WriteLine(i);
 }
 
-Console.WriteLine($"Product: {size} {color} {type}");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+
+string[] names = ["Alex", "Eddie", "David", "Michael"];
+for (var i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+*/
+var i = 0;
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+i = 0;
+foreach (var name in names)
+{
+    if (name == "Sammy")
+        names[i] = "Pepe";
+    
+    Console.WriteLine(name);
+    i++;
+}
