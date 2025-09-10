e829d8b744f0ec5b4cef5144520aab3ea1143d20
Juan Quijano <listas@juanquijano.es>
2025-09-10 10:05:25 +0000

    Ejercicio: Exploración de la interpolación de cadenas

--- Program.cs
+++ Program.cs
@@ -5,30 +5,14 @@ CultureInfo.CurrentCulture = new CultureInfo("es-ES");
 CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture;
 
 
-string first = "Hello";
-string second = "World";
-string result = string.Format("{0} {0} {1}!", first, second);
-Console.WriteLine(result);
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
 
-Console.WriteLine("---------");
-
-decimal price = 123.45m;
-int discount = 50;
-Console.WriteLine($"Price: {price:C} (Save {discount:C})");
-
-Console.WriteLine("---------");
-decimal measurement = 123456.78912m;
-Console.WriteLine($"Measurement: {measurement:N6} units");
-
-Console.WriteLine("---------");
-decimal tax = .36785m;
-Console.WriteLine($"Tax rate: {tax:P2}");
-
-Console.WriteLine("---------");
-price = 67.55m;
-decimal salePrice = 59.99m;
-string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
-string yourDiscount1 = $"You saved {price - salePrice:C2} off the regular {price:C2} price. ";
-Console.WriteLine(yourDiscount);
-Console.WriteLine(yourDiscount1);
-
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
