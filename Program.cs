79c056858431466f0e4ce690991a49bec9a6efe5
Juan Quijano <listas@juanquijano.es>
2025-09-10 08:02:59 +0000

    Ejercicio: Exploración de la conversión y la coerción de tipos de datos y TryParse

--- Program.cs
+++ Program.cs
@@ -1,20 +1,40 @@
-﻿Console.Clear();
+﻿using Microsoft.VisualBasic;
 
-int val_A = 2;
-int val_B = val_A;
-val_B = 5;
+Console.Clear();
+/*
+var first = 2;
+var second = "4";
+var result = first + second;
+Console.WriteLine(result);
 
-Console.WriteLine("--Value Types--");
-Console.WriteLine($"val_A: {val_A}");
-Console.WriteLine($"val_B: {val_B}");
+Console.WriteLine("-----");
 
-Console.WriteLine();
+decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal;
 
-int[] ref_A= new int[1];
-ref_A[0] = 2;
-int[] ref_B = ref_A;
-ref_B[0] = 5;
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float  : {myFloat}");
 
-Console.WriteLine("--Reference Types--");
-Console.WriteLine($"ref_A[0]: {ref_A[0]}");
-Console.WriteLine($"ref_B[0]: {ref_B[0]}");
+Console.WriteLine("-----");
+
+
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);
+
+Console.WriteLine("-----");*/
+
+string sfirst = "5";
+string ssecond = "a";
+var salida = 0;
+var salida1 = 0;
+
+if(int.TryParse(sfirst, out salida) && int.TryParse(ssecond, out salida1))
+{
+    int sum =  salida + salida1;
+    Console.WriteLine(sum);
+}
+else
+{ Console.WriteLine("Error: Uno de los valores no es un número válido.");}
+
