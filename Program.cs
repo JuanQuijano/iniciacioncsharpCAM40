f12b0683c28cfe147af3a1fa346cc15cab2ba951
Juan Quijano <listas@juanquijano.es>
2025-09-10 12:09:00 +0000

    Ejercicio: Crear métodos reutilizables

--- Program.cs
+++ Program.cs
@@ -5,15 +5,74 @@ CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("es-ES");
 CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("es-ES");
 
 
-void DisplayRandomNumbers()
+int[] times = [800, 1200, 1600, 2000];
+int diff = 0;
+
+Console.WriteLine("Enter current GMT");
+int currentGMT = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Current Medicine Schedule:");
+DisplayTimes();
+
+Console.WriteLine("Enter new GMT");
+int newGMT = Convert.ToInt32(Console.ReadLine());
+TimesCalculation();
+
+Console.WriteLine("New Medicine Schedule:");
+DisplayTimes();
+
+Console.WriteLine();
+
+#region Methods
+
+/* Format and display medicine times */
+void DisplayTimes()
 {
-    var random = new Random();
-    for (int i = 0; i < 10; i++)
+
+    foreach (int val in times)
     {
-        Console.WriteLine($"La tirada ha sido de: {random.Next(1, 101)} puntos.");
-    } 
+        string time = val.ToString();
+        int len = time.Length;
+
+        if (len >= 3)
+        { time = time.Insert(len - 2, ":"); }
+        else if (len == 2)
+        { time = time.Insert(0, "0:"); }
+        else
+        { time = time.Insert(0, "0:0"); }
+
+        Console.Write($"{time} ");
+    }
+
     Console.WriteLine();
 }
 
-Console.WriteLine("Generating random numbers:");
-DisplayRandomNumbers();
+/* Adjust the times by adding the difference, keeping the value within 24 hours */
+void AdjustTimes()
+{
+    for (int i = 0; i < times.Length; i++)
+    { times[i] = (times[i] + diff) % 2400; }
+}
+
+/* Calculate the time difference based on GMT values */
+void TimesCalculation()
+{
+
+    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+    {
+        Console.WriteLine("Invalid GMT");
+    }
+    else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+    {
+        diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+        AdjustTimes();
+    }
+    else
+    {
+        diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+        AdjustTimes();
+    }
+
+}
+
+#endregion
