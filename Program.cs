5faaf8f1fbabb8b891c9f314f348263ac98600c0
Juan Quijano <listas@juanquijano.es>
2025-09-10 09:02:32 +0000

    Ejercicio: Realización de un desafío para generar operaciones matemáticas como tipos de números específicos

--- Program.cs
+++ Program.cs
@@ -1,26 +1,23 @@
-﻿using System;
-using System.Globalization;
+﻿Console.Clear();
 
-Console.Clear();
+var value1 = 11;
+var value2 = 6.2m;
+var value3 = 4.3f;
 
-string[] values = ["12.3", "45", "ABC", "11", "DEF"];
+var decimalDivision1 = (decimal)value1 / value2;
+var result1 = (int)decimal.Round(decimalDivision1, 0, System.MidpointRounding.AwayFromZero);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
 
-var message = string.Empty;
-var total = 0.0;
+var value3AsDecimal = decimal.Parse(value3.ToString(System.Globalization.CultureInfo.InvariantCulture));
+var result2 = value2 / value3AsDecimal;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
-foreach (var valor in values)
-{
-    if (double.TryParse(valor, NumberStyles.Number, CultureInfo.InvariantCulture, out var number))
-    {
-        total += number;
-    }
-    else
-    {
-        message += valor;
-    }
-}
-
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {total.ToString(CultureInfo.InvariantCulture)}");
+var result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
 
 
+/*
+Divide value1 by value2, display the result as an int: 2
+Divide value2 by value3, display the result as a decimal: 1.4418604651162790697674418605
+Divide value3 by value1, display the result as a float: 0.3909091
+*/
