637a1d0aa61378f153fd4da7cba926d4163f7c33
Juan Quijano <listas@juanquijano.es>
2025-09-10 08:25:09 +0000

    Ejercicio: Realización de un desafío para combinar valores de matriz de cadenas como cadenas y como enteros

--- Program.cs
+++ Program.cs
@@ -1,40 +1,26 @@
-﻿using Microsoft.VisualBasic;
+﻿using System;
+using System.Globalization;
 
 Console.Clear();
-/*
-var first = 2;
-var second = "4";
-var result = first + second;
-Console.WriteLine(result);
 
-Console.WriteLine("-----");
+string[] values = ["12.3", "45", "ABC", "11", "DEF"];
 
-decimal myDecimal = 1.23456789m;
-float myFloat = (float)myDecimal;
+var message = string.Empty;
+var total = 0.0;
 
-Console.WriteLine($"Decimal: {myDecimal}");
-Console.WriteLine($"Float  : {myFloat}");
-
-Console.WriteLine("-----");
-
-
-int first = 5;
-int second = 7;
-string message = first.ToString() + second.ToString();
-Console.WriteLine(message);
-
-Console.WriteLine("-----");*/
-
-string sfirst = "5";
-string ssecond = "a";
-var salida = 0;
-var salida1 = 0;
-
-if(int.TryParse(sfirst, out salida) && int.TryParse(ssecond, out salida1))
+foreach (var valor in values)
 {
-    int sum =  salida + salida1;
-    Console.WriteLine(sum);
+    if (double.TryParse(valor, NumberStyles.Number, CultureInfo.InvariantCulture, out var number))
+    {
+        total += number;
+    }
+    else
+    {
+        message += valor;
+    }
 }
-else
-{ Console.WriteLine("Error: Uno de los valores no es un número válido.");}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total.ToString(CultureInfo.InvariantCulture)}");
+
 
