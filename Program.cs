3e00b9e536eb2ad3ef9fab186a67ee20fc32f60b
Juan Quijano <listas@juanquijano.es>
2025-09-11 07:58:34 +0000

    Creación de un método con parámetros

--- Program.cs
+++ Program.cs
@@ -4,23 +4,13 @@ Console.Clear();
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("es-ES");
 CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("es-ES");
 
-Random random = new Random();
-string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
-string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
-string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
-string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
+CountTo(7);
 
-Console.WriteLine("A fortune teller whispers the following words:");
-int luck = random.Next(100);
-MuestraElNivelDeSuerte();
-
-void MuestraElNivelDeSuerte()
+void CountTo(int max)
 {
-    string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < max; i++)
     {
-        Console.Write($@"{text[i]} {fortune[i]} ");
+        Console.Write($"{i}, ");
     }
+    Console.WriteLine();
 }
-
-
