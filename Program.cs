f127c1e32d00873f4c0604144858dbffdc9baa6e
Juan Quijano <listas@juanquijano.es>
2025-09-15 09:13:36 +0000

    Uso de propiedades

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿public class Person
+{
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; } = 0;
+
+    // Methods
+    public void Display()
+    {
+        Console.Clear();
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person();
+        person.Name = "Alice";
+        person.Age = 27;
+        
+        person.Display();
+    }
+}
